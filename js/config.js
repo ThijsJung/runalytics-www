950acd242455eaa01d6bba85b9d0da1f5e70ecab
Thijs Jung <thijs.jung@gmail.com>
2019-01-11 22:50:16 +0100

    Added authentication

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_pId8p2reO',
+        region: 'eu-west-1',
+		clientId: '316evmsegujtoi2qncltp50etv' //is this used anywhere?
+    }
+};
